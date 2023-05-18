4df4e6f8d12ba8eb1cbea104f8a4d51b6d89898b
RafaelIrabien <rafa.irabien@gmail.com>
2023-05-18 17:04:44 -0600

    Agregar archivo Config.php

---
+++ Config/config.php
@@ -0,0 +1,13 @@
+<?php 
+
+	const BASE_URL = "http://localhost/TIENDA_VIRTUAL/";
+	const HOST = "localhost";
+	const USER = "root";
+	const PASS = "";
+	const DB = "tienda_virtual";
+	const CHARSET = "charset=utf8";
+	const TITLE = "TIENDA VIRTUAL";
+	const MONEDA = "USD";
+	const CLIENT_ID = "";
+
+ ?>
